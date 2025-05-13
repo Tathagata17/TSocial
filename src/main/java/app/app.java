872b06c8830e154f1982d33dfe719fc154e03bcd
tathagata17 <b63user48@rpsconsulting.in>
2025-05-14 00:22:50 +0530

    Created app and commited changes

---
+++ src/main/java/app/app.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+
+public class app {
+
+	public static void main(String args[])
+	{
+		SpringApplication.run(app.class, args);
+	}
+}
